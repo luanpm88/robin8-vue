0406cba03e9dab7ee8d6ad8015fba9b9c276620f
luobinZhao <luobinzhao@luobinZhaodeMacBook-Pro.local>
2019-05-16 16:27:39 +0800

    修改kolsearch 缓存不清除的bug

--- src/pages/kolList/List.vue
+++ src/pages/kolList/List.vue
@@ -23,13 +23,16 @@ export default {
   beforeRouteLeave(to, from, next) {
     // 判断是下一个路由是不是进入到详情页
     if (to.name === 'KolDetail') {
-      this.$set(this.$route.meta, 'keepAlive', true)
-      // this.$route.meta.keepAlive = true
+      // console.log('我是详情页面')
+      if (!from.meta.keepAlive) {
+        from.meta.keepAlive = true
+      }
+      next()
     } else {
-      this.$set(this.$route.meta, 'keepAlive', false)
-      // this.$route.meta.keepAlive = false
+      from.meta.keepAlive = false
+      this.$destroy()
+      next()
     }
-    next()
   },
   created() {
     this.$route.meta.keepAlive = false
